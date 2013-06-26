1a37e5c08f88bab2cf77d22bdbc6329368fe50e3
Oliver Charles <ollie@ocharles.org.uk>
2013-06-26 16:59:30 +0100

    Make sure PostgreSQL has a cluster available

--- postgresql/recipes/default.rb
+++ postgresql/recipes/default.rb
@@ -12,11 +12,22 @@ end
 package "postgresql-musicbrainz-collate"
 package "postgresql-musicbrainz-unaccent"
 
+bash "Ensure Pg cluster" do
+  user "root"
+  code <<-EOH
+  if [ ! -d /var/lib/postgresql/9.1/main ]
+  then
+    rm -fr /etc/postgresql/9.1/main
+    pg_createcluster -d /var/lib/postgresql/9.1/main 9.1 main
+  fi
+  EOH
+end
+
 cookbook_file "/etc/postgresql/9.1/main/pg_hba.conf" do
   source "pg_hba.conf"
   owner "postgres"
 end
 
 service "postgresql" do
-  action [:start,:reload]
+  action [ :start, :reload ]
 end
