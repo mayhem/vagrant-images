3c3c6fa859871ddc416bd19a0f5356829a2c95fa
Oliver Charles <ollie@ocharles.org.uk>
2013-06-28 11:31:09 +0100

    Extend /dev/sdb provisioning to ensure /dev/sdb is present

--- virtualmachine/recipes/default.rb
+++ virtualmachine/recipes/default.rb
@@ -3,11 +3,18 @@ user "postgres"
 bash "partition /dev/sdb if necessary" do
   user "root"
   code <<-EOH
-  if [ ! -b /dev/sdb1 ]; then
-    sudo parted -s /dev/sdb mklabel msdos
-    sudo parted -s /dev/sdb -- mkpart primary 1 '-1'
-    sudo partprobe
-    sudo mkfs.ext4 -q /dev/sdb1 2>/dev/null
+  if [ -b /dev/sdb ]
+  then
+      if [ ! -b /dev/sdb1 ]
+      then
+          sudo parted -s /dev/sdb mklabel msdos
+          sudo parted -s /dev/sdb -- mkpart primary 1 '-1'
+          sudo partprobe
+          sudo mkfs.ext4 -q /dev/sdb1 2>/dev/null
+      fi
+  else
+      echo 'No /dev/sdb found - do you have the database drive mounted?'
+      exit 1
   fi
   EOH
 end 
